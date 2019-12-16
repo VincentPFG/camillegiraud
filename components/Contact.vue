0ae0bb42d777c5956805b1b7b7213d90a9d355a7
VincentPFG <vincentgiraud@live.fr>
2019-12-16 13:56:41 +0100

    second

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,21 +2,29 @@
 v-list
 	v-list-item
 		v-list-item-icon
-			v-icon mdi-phone
+			v-icon mdi-cellphone
 		v-list-item-content
-			v-list-item-title 54 54 35 46 35
+			v-list-item-title 06 70 13 08 03
+		v-list-item-action
+			v-btn(large icon)
+				v-icon mdi-content-copy
+	v-divider
 	v-list-item
 		v-list-item-icon
 			v-icon mdi-email
 		v-list-item-content
-			v-list-item-title ksjfzk@kjfhkjqh
+			v-list-item-title camille-giraud@orange.fr
 		v-list-item-action
 			Form
+	v-divider
 	v-list-item
 		v-list-item-icon
 			v-icon mdi-map-marker
 		v-list-item-content
-			v-list-item-title 254 Avenue de Monnom #[br] 68765 
+			v-list-item-title 11 Rue des Longines #[br] Écardenville sur Eure #[br] 27490 CLEF Vallée d'Eure
+		v-list-item-action
+			v-btn(large icon)
+				v-icon mdi-google-maps
 </template>
 
 <script lang='coffee'>
--- components/Form.vue
+++ components/Form.vue
@@ -1,26 +1,30 @@
 <template lang='pug'>
-v-dialog(v-model='dialog')
-    template(v-slot:activator='{on}')
-        v-layout(justify-center)
-            v-btn(v-on='on') contacter
-    v-form(netlify name='test-block')
-        div(hidden)
-            input(name='form-name' value='test-block')
-            input(name='de' :value='intro')
-            input(name='mail' :value='email')
-            textarea(name='message' :value='message')
-        v-container
-            v-layout(wrap)
-                v-flex(xs12 md2)
-                    v-select(:items='civilItems' label='civilité' v-model='civil')
-                v-flex(xs12 md5)
-                    v-text-field(v-model='name' label='name')
-                v-flex(xs12 md5)
-                    v-text-field(v-model='email' label='email')
-
-            v-textarea(v-model='message' label='message')
-            v-layout(justify-center)
-                    v-btn(type='submit') submit
+div
+    v-btn(@click.stop='dialog = true' large icon)
+        v-icon mdi-pencil
+    v-dialog(v-model='dialog')
+        //- template(v-slot:activator='{on}')
+        //-     v-layout(justify-center)
+        //-         v-btn(v-on='on') contacter
+        v-card
+            v-form(netlify name='contact')
+                div(hidden)
+                    input(name='form-name' value='contact')
+                    input(name='de' :value='intro')
+                    input(name='mail' :value='email')
+                    textarea(name='message' :value='message')
+                v-container
+                    v-row
+                        v-col(cols='12' md='2')
+                            v-select(:items='civilItems' label='civilité' v-model='civil')
+                        v-col(cols='12' md='5')
+                            v-text-field(v-model='name' label='name')
+                        v-col(cols='12' md='5')
+                            v-text-field(v-model='email' label='email')
+                    v-textarea(v-model='message' label='message')
+                    v-row(justify='center')
+                        v-btn(type='submit' large icon)
+                            v-icon mdi-send
 </template>
 
 <script lang='coffee'>
